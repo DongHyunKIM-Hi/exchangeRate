51776c5483863797bca9081229cd733d8f871c53
kongkdh <happybird00@naver.com>
2022-04-14 11:18:41 +0900

    feat: 스켈레톤 서버 셋팅

---
+++ src/main/kotlin/com/example/exchangerate/ExchangeRateApplication.kt
@@ -0,0 +1,12 @@
+package com.example.exchangerate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
+class ExchangeRateApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExchangeRateApplication>(*args)
+}
